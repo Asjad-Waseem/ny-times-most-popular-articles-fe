8cce926e5f094e8c50bf79128c8527a810e80980
asjad-waseem-neural-lab <asjad.waseem@neurallab.co>
2024-06-17 01:15:00 +0500

    PR-0002-setup-components-for-the-feeds-page

---
+++ src/components/buttons/dropdown/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { BiChevronDown } from "react-icons/bi";
+
+import { DropdownProps } from "./types";
+
+const Dropdown = ({
+  options,
+  selectedOption,
+  handleSelectedChange,
+}: DropdownProps): JSX.Element => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div
+      data-testid="dropdown-test"
+      className="font-medium h-40 border-gray-300 rounded"
+    >
+      <div
+        onClick={() => setOpen(!open)}
+        className={` bg-white w-full p-2 flex items-center border-b border-gray-500	 justify-between rounded ${
+          !selectedOption && "text-gray-700"
+        }`}
+      >
+        {selectedOption}
+        <BiChevronDown size={20} className={`${open && "rotate-180"}`} />
+      </div>
+      <ul
+        className={` bg-white mt-2 overflow-y-auto border-purple-900 ${
+          open ? "max-h-60" : "max-h-0"
+        } `}
+      >
+        {options?.map((option: number, index: number) => (
+          <li
+            key={index}
+            data-testid={`option-${option}`}
+            className={`p-2 text-sm hover:bg-gray-200
+            ${option === selectedOption && "bg-sky-600 text-white"}
+          `}
+            onClick={() => {
+              if (option !== selectedOption) {
+                handleSelectedChange(option);
+                setOpen(false);
+              }
+            }}
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Dropdown } from "./buttons/dropdown";
+export { default as ArticleCardContentBasic } from "./content/articleCardContentBasic";
+export { default as ArticleCardContentNested } from "./content/articleCardContentNested";
